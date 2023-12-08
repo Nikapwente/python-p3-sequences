4bfd46bb39275fd98f290b5246f7192657aec884
Nikapwente <hrsmajor@gmail.com>
2023-12-08 07:59:54 +0300

    Assignment Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,15 @@
 
 def print_fibonacci(length):
     pass
+    fib_sequence = [0,1]
+    if (length==0):
+        print([])
+    elif (length==1):
+        print([0])
+    elif (length ==2):
+        print(fib_sequence)
+    else:        
+        for x in range(2,length):
+            next_term = fib_sequence[x-1] + fib_sequence[x-2]
+            fib_sequence.append(next_term)
+        print(fib_sequence)
